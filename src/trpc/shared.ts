8963ee212f7405624b7f32c9ceae6e64751cff99
LarbeartoW <larbeartow@gmail.com>
2024-10-31 02:13:53 -0700

    small fix

--- src/trpc/shared.ts
+++ src/trpc/shared.ts
@@ -7,7 +7,7 @@ export const transformer = superjson
 function getBaseUrl() {
   if (typeof window !== 'undefined') return ''
   if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`
-  return `http://localhost:${process.env.PORT ?? 3000}`
+  return `http://localhost:${process.env.PORT ?? 3000} | 't3-nerds.vercel.app'`
 }
 
 export function getUrl() {

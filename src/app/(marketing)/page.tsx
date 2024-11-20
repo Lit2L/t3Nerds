f30c35b13768b0caca6785f49a31bc7bdc384567
LarbeartoW <larbeartow@gmail.com>
2024-11-19 18:30:54 -0800

    added location component

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,17 +1,17 @@
+import { AnimatedPinCard } from '@/components/cards/AnimatedPinCard'
 import { TrainingCards } from '@/components/sections/TrainingCards'
 import { Benefits } from '@/components/sections/benefits'
 import HeroLanding from '@/components/sections/hero-landing'
 import { Schedule } from '@/components/sections/schedule'
 
 export default async function IndexPage() {
-  const stars = false
-
   return (
     <div className='mx-auto min-h-screen w-full'>
       <HeroLanding />
       <TrainingCards />
       <Benefits />
       <Schedule />
+      <AnimatedPinCard />
     </div>
   )
 }
---
+++ src/components/cards/AnimatedPinCard.tsx
@@ -0,0 +1,49 @@
+'use client'
+
+import { Card, CardContent } from '@/components/ui/card'
+import { Separator } from '@/components/ui/separator'
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+export function AnimatedPinCard() {
+  return (
+    <div className='flex h-[50rem] w-full items-center justify-center'>
+      <Link
+        title='8780 Warner Ave, Suite 7 Fountain Valley, CA 92708'
+        href='https://maps.app.goo.gl/zvZ1hC5it3SM1qad7'
+        className='rounded-lg border bg-black p-3'
+      >
+        <div className='flex h-[20rem] w-[28rem] basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2'>
+          <p className='!m-0 max-w-xs !pb-2 font-heading text-base font-bold text-slate-100'>
+            Nerds Fighting
+          </p>
+          <span className=''>Location: Williams MMA</span>
+          <div className='!m-0 !p-0 text-base font-normal'>
+            <span className='text-slate-500'>
+              Address: 8780 Warner Ave, Suite 7 Fountain Valley, CA 92708
+            </span>
+          </div>
+          <div className='flex w-full rounded-lg p-3'>
+            <Image
+              src='/location.png'
+              alt='location'
+              width={420}
+              height={420}
+              className='rounded-md border'
+            />
+          </div>
+        </div>
+        <Card className='my-6 py-6'>
+          <CardContent className='p-3'>
+            <p className='text-center font-heading text-2xl'>
+              Location: Williams MMA
+            </p>
+            <Separator className='my-1' />
+            8780 Warner Ave, Suite 7 Fountain Valley, CA 92708
+          </CardContent>
+        </Card>
+      </Link>
+    </div>
+  )
+}
